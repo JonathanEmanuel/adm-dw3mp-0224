930fcc2644556033c751ca659a340ef22d62aade
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2024-09-06 12:28:32 -0300

    Clase 04 - Propiedades Computadas

---
+++ Clase_04/app.js
@@ -0,0 +1,78 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            tasks: [],
+            task: '',
+            filtro: '',
+            status: 1,
+        }
+    },
+    created(){
+        console.log('Creación de la instancia');
+        this.getTask();
+    },
+    computed:{
+        filtradas(){
+            const list = this.tasks.filter( task => task.name.toLowerCase().includes( this.filtro.toLowerCase() ));
+            console.log(list);
+            return list;
+        },
+        cantidad(){
+            return this.tasks.length;
+        },
+        completadas(){
+            
+        },
+        pendientes(){
+
+        }
+
+    },
+    methods: {
+        async getTask(){
+            try {
+                const response = await fetch('data/tasks.json');
+                const json = await response.json();
+                this.tasks = json;
+
+            } catch (error) {
+                Swal.fire({
+                    title: 'Error!',
+                    text: 'Upss ocurrio un error del Servidor',
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar'
+                })
+                console.error( error)
+            }
+        },
+/*         getTask(){
+            fetch('data/tasks.json')
+            .then( res => res.json())
+            .then( json => {
+                console.log(json);
+                this.tasks = json;
+            }).catch( error => {
+                console.error(error)
+            })
+        }, */
+        // Agrega una tarea el array
+        add(){
+            console.log('Nueva tarea');
+            const id = this.tasks.length + 1;
+            this.tasks.push({
+                id: id,
+                name: this.task,
+                computed: false
+            })
+        },
+        delete(){
+
+        },
+        update(){
+
+        }
+
+    }
+})
+
+app.mount('#app')
